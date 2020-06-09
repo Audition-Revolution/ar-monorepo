667205f23bc6a7241132c6871e446d377e897c6a
king0120 <king0120@gmail.com>
2020-06-09 12:31:56 -0400

    add batching to graphql

--- packages/client-ar/src/graphQLClient.ts
+++ packages/client-ar/src/graphQLClient.ts
@@ -1,18 +1,31 @@
-import ApolloClient from "apollo-boost";
-
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { BatchHttpLink } from "apollo-link-batch-http";
 const token = localStorage.getItem("accessToken");
 
 export default new ApolloClient({
-    headers: {
-        Authorization: token ? `Bearer ${token}` : ""
-    },
-    uri:
-        process.env.NODE_ENV === "production"
-            ? "https://aud-rev-test.herokuapp.com/graphql"
-            : undefined,
-    clientState: {
-        defaults: {},
-        resolvers: {},
-        typeDefs: ``
-    }
+    link: ApolloLink.from([
+        onError(({ graphQLErrors, networkError }) => {
+            if (graphQLErrors)
+                graphQLErrors.forEach(({ message, locations, path }) =>
+                    console.log(
+                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                    ),
+                );
+            if (networkError) console.log(`[Network error]: ${networkError}`);
+        }),
+        new BatchHttpLink({
+            uri: process.env.NODE_ENV === "production"
+                ? "https://aud-rev-test.herokuapp.com/graphql"
+                : undefined,
+            credentials: 'same-origin',
+            headers: {
+                Authorization: token ? `Bearer ${token}` : ""
+            },
+        })
+    ]),
+    cache: new InMemoryCache()
 });
